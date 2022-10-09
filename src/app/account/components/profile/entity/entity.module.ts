abe0a9dc9b20d0b650e7e0d0ae40d08f86b5fe8b
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-10-09 13:52:22 +1000

    feat: profile refactored

--- src/app/account/components/profile/entity/entity.module.ts
+++ src/app/account/components/profile/entity/entity.module.ts
@@ -1,17 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EntityComponent } from './components/entity/entity.component';
-import { EntityEditComponent } from './components/entity-edit/entity-edit.component';
-
-
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {EntityComponent} from './components/entity/entity.component'
+import {EntityEditComponent} from './components/entity-edit/entity-edit.component'
 
 @NgModule({
-  declarations: [
-    EntityComponent,
-    EntityEditComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [EntityComponent, EntityEditComponent],
+  imports: [CommonModule],
 })
-export class EntityModule { }
+export class EntityModule {}
