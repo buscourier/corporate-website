b8a0eb8ada265e3b472fd2a3450b14f3b8f41dd4
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-10-09 19:07:12 +1000

    feat: profile refactored

--- src/app/account/components/profile/profile.component.ts
+++ src/app/account/components/profile/profile.component.ts
@@ -1,16 +1,28 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core'
+import {TUI_SVG_SRC_PROCESSOR} from '@taiga-ui/core'
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: TUI_SVG_SRC_PROCESSOR,
+      useFactory: () => {
+        return (src: string): string => {
+          const myCustomPrefix = `icons::`
+
+          return src.startsWith(myCustomPrefix)
+            ? `assets/icons/${src.replace(myCustomPrefix, ``)}.svg`
+            : src
+        }
+      },
+    },
+  ],
 })
 export class ProfileComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/account/components/profile/profile.module.ts
+++ src/app/account/components/profile/profile.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {ProfileComponent} from './profile.component'
 import {ProfileRoutingModule} from './profile-routing.module'
+import {TuiSvgModule} from '@taiga-ui/core'
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, TuiSvgModule],
 })
 export class ProfileModule {}
