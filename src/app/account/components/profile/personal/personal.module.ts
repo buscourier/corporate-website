a9d1d72f8fe087055d4fba5500858b83edbf7155
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-10-09 20:03:57 +1000

    feat: markup for personal info done

--- src/app/account/components/profile/personal/personal.module.ts
+++ src/app/account/components/profile/personal/personal.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common'
 import {PersonalComponent} from './components/personal/personal.component'
 import {PersonalEditComponent} from './components/personal-edit/personal-edit.component'
 import {PersonalRoutingModule} from './personal-routing.module'
+import {TuiButtonModule} from '@taiga-ui/core'
 
 @NgModule({
   declarations: [PersonalComponent, PersonalEditComponent],
-  imports: [CommonModule, PersonalRoutingModule],
+  imports: [CommonModule, PersonalRoutingModule, TuiButtonModule],
 })
 export class PersonalModule {}
