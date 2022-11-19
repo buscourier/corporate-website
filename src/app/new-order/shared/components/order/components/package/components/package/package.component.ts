ebbf3b2a5d333d04a8c62e4b0b7df3170c4ed9f8
a.mayatskiy <work.mayatskiy@gmail.com>
2022-11-19 14:07:58 +1000

    feat: modal for packages add

--- src/app/new-order/shared/components/order/components/package/components/package/package.component.ts
+++ src/app/new-order/shared/components/order/components/package/components/package/package.component.ts
@@ -1,11 +1,23 @@
-import {ChangeDetectionStrategy, Component, Inject, OnInit} from '@angular/core'
-import {FormBuilder} from '@angular/forms'
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core'
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms'
 import {Store} from '@ngrx/store'
 import {concatAll, filter, map, Observable, of, toArray} from 'rxjs'
-import {switchMap} from 'rxjs/operators'
+import {switchMap, tap} from 'rxjs/operators'
 import {allServicesSelector} from 'src/app/new-order/shared/components/orders/store/selectors'
 import {ServiceInterface} from '../../../../../../types/service.interface'
-import {TuiDialogService} from '@taiga-ui/core'
+import {TuiDialogContext, TuiDialogService} from '@taiga-ui/core'
+import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus'
+
+// interface PackageInterface {
+//   [key: string]: boolean
+//   count: number,
+// }
 
 @Component({
   selector: 'app-package',
@@ -16,17 +28,17 @@ import {TuiDialogService} from '@taiga-ui/core'
 export class PackageComponent implements OnInit {
   services$: Observable<ServiceInterface[]>
 
-  box = this.fb.array([])
-  safePack = this.fb.array([])
-  placticPack = this.fb.array([])
-  skin = this.fb.array([])
-  other = this.fb.array([])
+  boxes = this.fb.array<FormGroup>([])
+  safePacks = this.fb.array<FormGroup>([])
+  placticPacks = this.fb.array<FormGroup>([])
+  skins = this.fb.array<FormGroup>([])
+  other = this.fb.array<FormGroup>([])
 
   form = this.fb.group({
-    box: this.box,
-    safePack: this.safePack,
-    placticPack: this.placticPack,
-    skin: this.skin,
+    boxes: this.boxes,
+    safePacks: this.safePacks,
+    placticPacks: this.placticPacks,
+    skins: this.skins,
     other: this.other,
   })
 
@@ -34,30 +46,96 @@ export class PackageComponent implements OnInit {
     private fb: FormBuilder,
     private store: Store,
     @Inject(TuiDialogService)
-    private readonly dialogService: TuiDialogService
+    private readonly dialogService: TuiDialogService,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
     this.initializeValues()
+    console.log('this.form', this.form)
   }
 
   initializeValues() {
-    this.services$ = this.store.select(allServicesSelector).pipe(
-      filter(Boolean),
-      switchMap((services: ServiceInterface[]) => {
-        return of(services).pipe(
-          concatAll(),
-          filter((service: ServiceInterface) => service.group_id === '1'),
-          toArray(),
-          map((services: ServiceInterface[]) => {
-            return services
-          })
-        )
-      })
-    )
+    this.store
+      .select(allServicesSelector)
+      .pipe(
+        filter(Boolean),
+        switchMap((services: ServiceInterface[]) => {
+          return of(services).pipe(
+            concatAll(),
+            filter((service: ServiceInterface) => service.group_id === '1'),
+            toArray(),
+            map((services: ServiceInterface[]) => {
+              services.forEach((service: ServiceInterface) => {
+                switch (service.subgroup_id) {
+                  case '1':
+                    this.boxes.push(
+                      this.fb.group({
+                        [service.id]: false,
+                        count: 0,
+                        data: {...service, short_name: 'Коробка'},
+                      })
+                    )
+                    break
+                  case '2':
+                    this.safePacks.push(
+                      this.fb.group({
+                        [service.id]: false,
+                        count: 0,
+                        data: service,
+                      })
+                    )
+                    break
+                  case '3':
+                    this.placticPacks.push(
+                      this.fb.group({
+                        [service.id]: false,
+                        count: 0,
+                        data: service,
+                      })
+                    )
+                    break
+                  case '6':
+                    this.skins.push(
+                      this.fb.group({
+                        [service.id]: false,
+                        count: 0,
+                        data: service,
+                      })
+                    )
+                    break
+                  default:
+                    this.other.push(
+                      this.fb.group({
+                        [service.id]: false,
+                        count: 0,
+                        data: service,
+                      })
+                    )
+                    break
+                }
+              })
+              return services
+            })
+          )
+        }),
+        tap(() => {
+          this.cdr.markForCheck()
+        })
+      )
+      .subscribe()
+  }
+
+  getControl(control) {
+    return control as FormControl
   }
 
   open() {
-    this.dialogService.open('hello').subscribe()
+    console.log('form', this.form.value)
+    // this.dialogService.open('hello').subscribe()
+  }
+
+  openDialog(content: PolymorpheusContent<TuiDialogContext>) {
+    this.dialogService.open(content).subscribe()
   }
 }
--- src/app/new-order/shared/components/order/components/package/package.module.ts
+++ src/app/new-order/shared/components/order/components/package/package.module.ts
@@ -1,11 +1,25 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {PackageComponent} from './components/package/package.component'
-import {TuiButtonModule, TuiDialogModule} from '@taiga-ui/core'
+import {
+  TuiButtonModule,
+  TuiDialogModule,
+  TuiTextfieldControllerModule,
+} from '@taiga-ui/core'
+import {ReactiveFormsModule} from '@angular/forms'
+import {TuiCheckboxLabeledModule, TuiInputCountModule} from '@taiga-ui/kit'
 
 @NgModule({
   declarations: [PackageComponent],
-  imports: [CommonModule, TuiDialogModule, TuiButtonModule],
+  imports: [
+    CommonModule,
+    TuiDialogModule,
+    TuiButtonModule,
+    ReactiveFormsModule,
+    TuiCheckboxLabeledModule,
+    TuiInputCountModule,
+    TuiTextfieldControllerModule,
+  ],
   exports: [PackageComponent],
 })
 export class PackageModule {}
