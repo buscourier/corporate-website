c00c6143e46df1ed4ada63bf9bc650a57286093a
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-11-25 22:52:16 +1000

    feat: validation for steps done

--- src/app/new-order/components/checkout/components/step-three/step-three.component.ts
+++ src/app/new-order/components/checkout/components/step-three/step-three.component.ts
@@ -1,4 +1,15 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {combineLatest, map, Subscription} from 'rxjs'
+import {isEndPointValidSelector} from 'src/app/new-order/shared/components/end-point/store/selectors'
+import {isOrdersValidSelector} from '../../../../shared/components/orders/store/selectors'
+import {setCurrentStepStateAction} from '../../store/actions/set-current-step-state.action'
+import {isRecipientValidSelector} from './components/recipient/store/selectors'
 
 @Component({
   selector: 'app-step-three',
@@ -6,4 +17,30 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   styleUrls: ['./step-three.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StepThreeComponent {}
+export class StepThreeComponent implements OnInit, OnDestroy {
+  combineAllSub: Subscription
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.combineAllSub = combineLatest([
+      this.store.select(isEndPointValidSelector),
+      this.store.select(isOrdersValidSelector),
+      this.store.select(isRecipientValidSelector),
+    ])
+      .pipe(
+        map(([isEndpointValid, isOrdersValid, isRecipientValid]) => {
+          return this.store.dispatch(
+            setCurrentStepStateAction({
+              isValid: isEndpointValid && isOrdersValid && isRecipientValid,
+            })
+          )
+        })
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy(): void {
+    this.combineAllSub.unsubscribe()
+  }
+}
--- src/app/new-order/components/checkout/components/step-two/step-two.component.ts
+++ src/app/new-order/components/checkout/components/step-two/step-two.component.ts
@@ -1,8 +1,43 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {combineLatest, map, Subscription} from 'rxjs'
+import {isStartPointValidSelector} from '../../../../shared/components/start-point/store/selectors'
+import {setCurrentStepStateAction} from '../../store/actions/set-current-step-state.action'
+import {isSenderValidSelector} from './components/sender/store/selectors'
 
 @Component({
   selector: 'app-step-two',
   templateUrl: './step-two.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StepTwoComponent {}
+export class StepTwoComponent implements OnInit, OnDestroy {
+  combineAllSub: Subscription
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.combineAllSub = combineLatest([
+      this.store.select(isSenderValidSelector),
+      this.store.select(isStartPointValidSelector),
+    ])
+      .pipe(
+        map(([isSenderValid, isStartPointValid]) => {
+          return this.store.dispatch(
+            setCurrentStepStateAction({
+              isValid: isSenderValid && isStartPointValid,
+            })
+          )
+        })
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy(): void {
+    this.combineAllSub.unsubscribe()
+  }
+}
