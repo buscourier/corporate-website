03a012f25da32a3f4235ab2b10f368cc70a9db9a
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-11-29 07:41:48 +1000

    feat: add services

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
       xl: ['1.75rem', {lineHeight: '2.1rem'}], // 28 / 33.6
       '2xl': ['2rem', {lineHeight: '2.4rem'}], // 32 / 38.4
       '3xl': ['2.625rem', {lineHeight: '3.25rem'}], // 42 / 52
-      '4xl': ['3.563rem', {lineHeight: '4.25rem'}], // 42 / 52
+      '4xl': ['3.563rem', {lineHeight: '4.25rem'}], // 57 / 52
     },
     borderRadius: {
       none: '0px',
