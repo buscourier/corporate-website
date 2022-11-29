066c7d4f39f32f70c4044ad645e93e960983854a
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-11-30 04:14:59 +1000

    feat: filter in my orders refactored

--- src/app/account/components/orders/components/filter/components/filter/filter.component.ts
+++ src/app/account/components/orders/components/filter/components/filter/filter.component.ts
@@ -8,42 +8,24 @@ import {
 } from '@angular/core'
 import {FormBuilder} from '@angular/forms'
 import {Store} from '@ngrx/store'
-import {tuiItemsHandlersProvider} from '@taiga-ui/kit'
 import {Observable, Subscription} from 'rxjs'
-import {tap} from 'rxjs/operators'
-import {STRINGIFY_CITIES} from '../../../../../../../shared/handlers/string-handlers'
-import {EndCityInterface} from '../../../../../../../shared/types/end-city.interface'
-import {StartCityInterface} from '../../../../../../../shared/types/start-city.interface'
-import {getEndCitiesAction} from '../../store/actions/get-end-cities.action'
-import {getStartCitiesAction} from '../../store/actions/get-start-cities.action'
-import {
-  backendErrorsSelector,
-  endCitiesSelector,
-  isEndCitiesLoadingSelector,
-  isStartCitiesLoadingSelector,
-  startCitiesSelector,
-} from '../../store/selectors'
+import {backendErrorsSelector} from '../../store/selectors'
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css'],
-  providers: [tuiItemsHandlersProvider({stringify: STRINGIFY_CITIES})],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterComponent implements OnInit, OnDestroy {
-  isStartCitiesLoading$: Observable<boolean>
-  isEndCitiesLoading$: Observable<boolean>
-  startCities$: Observable<StartCityInterface[]>
-  endCities$: Observable<EndCityInterface[]>
   backendErrors$: Observable<null | string>
+  statuses = ['Готов к выдаче', 'Подготовка к отправке', 'Возврат', 'Испорчено']
 
   @Output('filterChanged') filterChangedEvent = new EventEmitter<any>()
 
   form = this.fb.group({
     range: [],
-    startCity: null,
-    endCity: {value: null, disabled: true},
+    status: null,
   })
 
   valueChangesSub: Subscription
@@ -52,7 +34,6 @@ export class FilterComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.initializeValues()
-    this.fetchData()
   }
 
   ngOnDestroy(): void {
@@ -60,26 +41,7 @@ export class FilterComponent implements OnInit, OnDestroy {
   }
 
   initializeValues(): void {
-    this.isStartCitiesLoading$ = this.store.select(isStartCitiesLoadingSelector)
-    this.isEndCitiesLoading$ = this.store.select(isEndCitiesLoadingSelector)
-    this.startCities$ = this.store.select(startCitiesSelector)
-    this.endCities$ = this.store.select(endCitiesSelector)
     this.backendErrors$ = this.store.select(backendErrorsSelector)
-
-    this.valueChangesSub = this.form
-      .get('startCity')
-      .valueChanges.pipe(
-        tap(({id}: StartCityInterface) => {
-          this.form.get('endCity').patchValue('')
-          this.form.get('endCity').enable()
-          this.store.dispatch(getEndCitiesAction({cityId: id}))
-        })
-      )
-      .subscribe()
-  }
-
-  fetchData(): void {
-    this.store.dispatch(getStartCitiesAction())
   }
 
   onSubmit() {
