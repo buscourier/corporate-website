f3648d7bdd90b114c99edab2bbc60c5efb88528b
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-11-29 19:56:30 +1000

    feat: hot fixes

--- src/app/index/components/examples/examples.module.ts
+++ src/app/index/components/examples/examples.module.ts
@@ -1,11 +1,24 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
-import {TuiCarouselModule} from '@taiga-ui/kit'
+import {TuiMoneyModule} from '@taiga-ui/addon-commerce'
+import {TuiLoaderModule} from '@taiga-ui/core'
+import {
+  TuiCarouselModule,
+  TuiIslandModule,
+  TuiMarkerIconModule,
+} from '@taiga-ui/kit'
 import {ExamplesComponent} from './examples.component'
 
 @NgModule({
   declarations: [ExamplesComponent],
-  imports: [CommonModule, TuiCarouselModule],
+  imports: [
+    CommonModule,
+    TuiCarouselModule,
+    TuiIslandModule,
+    TuiMoneyModule,
+    TuiLoaderModule,
+    TuiMarkerIconModule,
+  ],
   exports: [ExamplesComponent],
 })
 export class ExamplesModule {}
