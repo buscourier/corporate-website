885a03db7a16bcca1fc7c078da4eb2bc221edd87
a.mayatskiy <work.mayatskiy@gmail.com>
2022-12-02 20:02:33 +1000

    feat: state changed in index calculator

--- src/app/index/components/calculator/store/state.ts
+++ src/app/index/components/calculator/store/state.ts
@@ -5,6 +5,7 @@ export const CALCULATOR_FEATURE = 'indexCalculator'
 export const initialState: CalculatorStateInterface = {
   isStartCitiesLoading: false,
   isEndCitiesLoading: false,
+  isSubmitting: false,
   startCities: null,
   endCities: null,
   backendErrors: null,
--- src/app/index/components/calculator/types/calculator-state.interface.ts
+++ src/app/index/components/calculator/types/calculator-state.interface.ts
@@ -4,6 +4,7 @@ import {StartCityInterface} from '../../../../shared/types/start-city.interface'
 export interface CalculatorStateInterface {
   isStartCitiesLoading: boolean
   isEndCitiesLoading: boolean
+  isSubmitting: boolean
   startCities: StartCityInterface[] | null
   endCities: EndCityInterface[] | null
   backendErrors: string | null
