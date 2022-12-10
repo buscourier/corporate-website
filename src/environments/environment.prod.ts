aa96b5dd94dcf3c6ec1faaa767d0ff6404a29fe8
a.mayatskiy <work.mayatskiy@gmail.com>
2022-12-10 13:32:40 +1000

    feat: add apiKey for yandex map

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.busbox.guru/v1',
   apiKey: '8aab09f6-c5b3-43be-8895-153ea164984e',
+  apiMapKey: 'c8412bbb-d3e9-466b-994d-dd1b20fa7f5f',
   limit: 10,
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.busbox.guru/v1',
   apiKey: '8aab09f6-c5b3-43be-8895-153ea164984e',
+  apiMapKey: 'c8412bbb-d3e9-466b-994d-dd1b20fa7f5f',
   limit: 10,
 }
 
