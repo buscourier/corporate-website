b08680f3714509c15c887d58e08c4c3536153931
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-12 12:06:26 +1000

    feat: mobile map add to contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,10 +1,20 @@
-import {ChangeDetectionStrategy, Component, Inject, OnInit} from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  Injector,
+  OnInit,
+} from '@angular/core'
 import {FormControl} from '@angular/forms'
 import {Store} from '@ngrx/store'
 import {TuiDialogContext, TuiDialogService} from '@taiga-ui/core'
-import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus'
+import {
+  PolymorpheusComponent,
+  PolymorpheusContent,
+} from '@tinkoff/ng-polymorpheus'
 import {Observable, Subscription, take} from 'rxjs'
 import {tap} from 'rxjs/operators'
+import {ModalMapComponent} from '../shared/components/modal-map/modal-map.component'
 import {
   isLargeScreenSelector,
   mdScreenSelector,
@@ -34,7 +44,8 @@ export class ContactsComponent implements OnInit {
   constructor(
     private store: Store,
     @Inject(TuiDialogService)
-    private readonly dialogService: TuiDialogService
+    private readonly dialogService: TuiDialogService,
+    @Inject(Injector) private readonly injector: Injector
   ) {}
 
   ngOnInit(): void {
@@ -102,6 +113,28 @@ export class ContactsComponent implements OnInit {
       observer.complete()
     } else {
       this.detailsOpened = false
+    }
+  }
+
+  showOnMap(isMobile) {
+    if (isMobile) {
+      this.dialogService
+        .open<any>(
+          new PolymorpheusComponent(ModalMapComponent, this.injector),
+          {
+            data: {
+              address: 'Address',
+              points: [{geo_x: 0, geo_y: 0}],
+            },
+            dismissible: true,
+            closeable: false,
+            size: 'fullscreen',
+          }
+        )
+        .pipe(take(1))
+        .subscribe()
+    } else {
+      console.log('not mobile')
     }
   }
 }
