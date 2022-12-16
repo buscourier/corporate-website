3f4e43287e3b8f50baf57655a1cbc99fda24c942
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-16 13:38:46 +1000

    feat: sliders in eshop page done

--- src/app/shops/shops.component.ts
+++ src/app/shops/shops.component.ts
@@ -1,4 +1,13 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {Observable} from 'rxjs'
+import {tap} from 'rxjs/operators'
+import {screenSizeSelector} from '../store/global/selectors'
 
 @Component({
   selector: 'app-shops',
@@ -6,7 +15,9 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   styleUrls: ['./shops.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopsComponent {
+export class ShopsComponent implements OnInit {
+  screenSize$: Observable<string>
+
   shopIndex = 0
   shopsCount = 4
   pointsIndex = 0
@@ -16,7 +27,39 @@ export class ShopsComponent {
 
   public readonly points = [`iml`, `boxberry`, `hermes`, `kce`]
 
-  constructor() {}
+  constructor(private store: Store, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.store
+      .select(screenSizeSelector)
+      .pipe(
+        tap((size: string) => {
+          switch (size) {
+            case 'xs':
+              this.shopsCount = 1
+              this.pointsCount = 1
+              break
+            case 'sm':
+              this.shopsCount = 2
+              this.pointsCount = 2
+              break
+            case 'md':
+              this.shopsCount = 3
+              this.pointsCount = 3
+              break
+            case 'lg':
+              this.shopsCount = 4
+              this.pointsCount = 4
+              break
+          }
+
+          this.shopIndex = 0
+        })
+      )
+      .subscribe(() => {
+        this.cdr.markForCheck()
+      })
+  }
 
   onShopIndex(index: number): void {
     this.shopIndex = index
