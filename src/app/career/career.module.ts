e0c793d0e4348760d2e6d79e8dbb8cbad3e3bf41
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-17 20:59:08 +1000

    feat: add lists to career page

--- src/app/career/career.module.ts
+++ src/app/career/career.module.ts
@@ -1,11 +1,12 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {TuiSvgModule} from '@taiga-ui/core'
 import {ImgModule} from '../shared/components/img/img.module'
 import {CareerRoutingModule} from './career-routing.module'
 import {CareerComponent} from './career.component'
 
 @NgModule({
   declarations: [CareerComponent],
-  imports: [CommonModule, CareerRoutingModule, ImgModule],
+  imports: [CommonModule, CareerRoutingModule, ImgModule, TuiSvgModule],
 })
 export class CareerModule {}
