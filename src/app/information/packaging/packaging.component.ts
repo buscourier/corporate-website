80f6bd7d34036cd7ab5c9ba57d4c61f545e4710a
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-17 10:49:49 +1000

    feat: add images to packaging page

--- src/app/information/packaging/packaging.component.ts
+++ src/app/information/packaging/packaging.component.ts
@@ -1,4 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core'
+import {DomSanitizer} from '@angular/platform-browser'
+import {TUI_IS_MOBILE} from '@taiga-ui/cdk'
+import {TuiPdfViewerService} from '@taiga-ui/kit'
 
 @Component({
   selector: 'app-packaging',
@@ -7,5 +10,23 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PackagingComponent {
-  constructor() {}
+  constructor(
+    @Inject(DomSanitizer) private readonly sanitizer: DomSanitizer,
+    @Inject(TuiPdfViewerService)
+    private readonly pdfService: TuiPdfViewerService,
+    @Inject(TUI_IS_MOBILE) private readonly isMobile: boolean
+  ) {}
+
+  showPdf(): void {
+    this.pdfService
+      .open(
+        this.sanitizer.bypassSecurityTrustResourceUrl(
+          `https://busbox.guru/uploads/pages/Расписание_отправлений_Владивосток.pdf`
+        ),
+        {
+          label: `Правила приемки и отправки грузов`,
+        }
+      )
+      .subscribe()
+  }
 }
