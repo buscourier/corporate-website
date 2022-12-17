95988eaae8827161fbd191b4061fd04421ec4a1e
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-17 12:35:55 +1000

    feat: examples finsihed in complex-tasks page

--- src/app/services/complex-tasks/complex-tasks.module.ts
+++ src/app/services/complex-tasks/complex-tasks.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {ImgModule} from '../../shared/components/img/img.module'
 import {ComplexTasksRoutingModule} from './complex-tasks-routing.module'
 import {ComplexTasksComponent} from './complex-tasks.component'
 
 @NgModule({
   declarations: [ComplexTasksComponent],
-  imports: [CommonModule, ComplexTasksRoutingModule],
+  imports: [CommonModule, ComplexTasksRoutingModule, ImgModule],
 })
 export class ComplexTasksModule {}
