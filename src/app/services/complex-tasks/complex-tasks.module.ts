3eb2233afe8d61fa76afc9f8989df492e0152165
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-17 13:01:51 +1000

    feat: add images to complex tasks

--- src/app/services/complex-tasks/complex-tasks.module.ts
+++ src/app/services/complex-tasks/complex-tasks.module.ts
@@ -1,11 +1,17 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {ImgModule} from '../../shared/components/img/img.module'
+import {SupportFormModule} from '../../shared/components/support-form/support-form.module'
 import {ComplexTasksRoutingModule} from './complex-tasks-routing.module'
 import {ComplexTasksComponent} from './complex-tasks.component'
 
 @NgModule({
   declarations: [ComplexTasksComponent],
-  imports: [CommonModule, ComplexTasksRoutingModule, ImgModule],
+  imports: [
+    CommonModule,
+    ComplexTasksRoutingModule,
+    ImgModule,
+    SupportFormModule,
+  ],
 })
 export class ComplexTasksModule {}
