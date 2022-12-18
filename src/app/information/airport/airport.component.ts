ba6abe5f9b8a869a70b4051241bae976c0001dc9
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-18 15:37:05 +1000

    feat: aeroflot page done

--- src/app/information/airport/airport.component.ts
+++ src/app/information/airport/airport.component.ts
@@ -1,4 +1,12 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {Observable} from 'rxjs'
+import {tap} from 'rxjs/operators'
+import {screenSizeSelector} from '../../store/global/selectors'
 
 @Component({
   selector: 'app-airport',
@@ -7,5 +15,60 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AirportComponent {
-  constructor() {}
+  screenSize$: Observable<string>
+
+  pointsIndex = 0
+  pointsCount = 4
+
+  public readonly points = [
+    {
+      name: 'aeroflot-2',
+      link: 'https://www.aeroflot.ru/',
+    },
+    {
+      name: 'aurora',
+      link: 'https://www.flyaurora.ru',
+    },
+    {
+      name: 'eastjet',
+    },
+    {
+      name: 's7',
+      link: 'https://www.s7.ru/',
+    },
+  ]
+
+  constructor(private store: Store, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.store
+      .select(screenSizeSelector)
+      .pipe(
+        tap((size: string) => {
+          switch (size) {
+            case 'xs':
+              this.pointsCount = 1
+              break
+            case 'sm':
+              this.pointsCount = 2
+              break
+            case 'md':
+              this.pointsCount = 3
+              break
+            case 'lg':
+              this.pointsCount = 4
+              break
+          }
+
+          this.pointsIndex = 0
+        })
+      )
+      .subscribe(() => {
+        this.cdr.markForCheck()
+      })
+  }
+
+  onPointsIndex(index: number): void {
+    this.pointsIndex = index
+  }
 }
--- src/app/information/airport/airport.module.ts
+++ src/app/information/airport/airport.module.ts
@@ -1,11 +1,18 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {TuiSvgModule} from '@taiga-ui/core'
+import {TuiCarouselModule, TuiPaginationModule} from '@taiga-ui/kit'
 import {AirportRoutingModule} from './airport-routing.module'
 import {AirportComponent} from './airport.component'
 
 @NgModule({
   declarations: [AirportComponent],
-  imports: [CommonModule, AirportRoutingModule, TuiSvgModule],
+  imports: [
+    CommonModule,
+    AirportRoutingModule,
+    TuiSvgModule,
+    TuiPaginationModule,
+    TuiCarouselModule,
+  ],
 })
 export class AirportModule {}
