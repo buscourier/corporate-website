a0b3b27621a6c6722226e7cd9d25fd5fcd3f5143
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-18 21:45:12 +1000

    feat: hot-to-get page done

--- src/app/information/how-to-get/how-to-get.component.ts
+++ src/app/information/how-to-get/how-to-get.component.ts
@@ -1,4 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core'
+import {DomSanitizer} from '@angular/platform-browser'
+import {TUI_IS_MOBILE} from '@taiga-ui/cdk'
+import {TuiPdfViewerOptions, TuiPdfViewerService} from '@taiga-ui/kit'
+import {PolymorpheusContent} from '@tinkoff/ng-polymorpheus'
 
 @Component({
   selector: 'app-how-to-get',
@@ -7,5 +11,24 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HowToGetComponent {
-  constructor() {}
+  constructor(
+    @Inject(DomSanitizer) private readonly sanitizer: DomSanitizer,
+    @Inject(TuiPdfViewerService)
+    private readonly pdfService: TuiPdfViewerService,
+    @Inject(TUI_IS_MOBILE) private readonly isMobile: boolean
+  ) {}
+
+  showPdf(actions: PolymorpheusContent<TuiPdfViewerOptions>): void {
+    this.pdfService
+      .open(
+        this.sanitizer.bypassSecurityTrustResourceUrl(
+          `https://busbox.guru/uploads/pages/Правила_приёмки_и_отправки_грузов_Баскурьер.pdf`
+        ),
+        {
+          label: `Правила приемки и отправки грузов`,
+          actions,
+        }
+      )
+      .subscribe()
+  }
 }
--- src/app/information/how-to-get/how-to-get.module.ts
+++ src/app/information/how-to-get/how-to-get.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {TuiButtonModule, TuiSvgModule} from '@taiga-ui/core'
 import {HowToGetRoutingModule} from './hot-to-get-routing.module'
 import {HowToGetComponent} from './how-to-get.component'
 
 @NgModule({
   declarations: [HowToGetComponent],
-  imports: [CommonModule, HowToGetRoutingModule],
+  imports: [CommonModule, HowToGetRoutingModule, TuiSvgModule, TuiButtonModule],
 })
 export class HowToGetModule {}
