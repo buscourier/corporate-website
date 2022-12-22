5081e02e0d754c569ed9fb4496043d83bd3570d3
Mayatskiy Anton <work.mayatskiy@gmail.com>
2022-12-23 05:28:07 +1000

    fix: hot fixes

--- src/app/find-order/find-order.module.ts
+++ src/app/find-order/find-order.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core'
 import {ReactiveFormsModule} from '@angular/forms'
 import {EffectsModule} from '@ngrx/effects'
 import {StoreModule} from '@ngrx/store'
-import {TuiLetModule} from '@taiga-ui/cdk'
+import {TuiAutoFocusModule, TuiLetModule} from '@taiga-ui/cdk'
 import {
   TuiButtonModule,
   TuiLoaderModule,
@@ -11,13 +11,13 @@ import {
   TuiTextfieldControllerModule,
 } from '@taiga-ui/core'
 import {TuiInputModule} from '@taiga-ui/kit'
+import {ImgModule} from '../shared/components/img/img.module'
 import {FindOrderRoutingModule} from './find-order-routing.module'
 import {FindOrderComponent} from './find-order.component'
 import {FindOrderService} from './services/find-order.service'
 import {GetStatusesEffect} from './store/effects/get-statuses.effect'
 import {reducer} from './store/reducer'
 import {FIND_ORDER_FEATURE} from './store/state'
-import {ImgModule} from '../shared/components/img/img.module'
 
 @NgModule({
   declarations: [FindOrderComponent],
@@ -34,6 +34,7 @@ import {ImgModule} from '../shared/components/img/img.module'
     TuiLetModule,
     ImgModule,
     TuiSvgModule,
+    TuiAutoFocusModule,
   ],
   providers: [FindOrderService],
 })
