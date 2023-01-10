03df096a4a4ecaf66e6d4e2b54ce7385f805bc16
a.mayatskiy <work.mayatskiy@gmail.com>
2023-01-10 14:34:10 +1000

    feat: orders-success page done

--- src/app/new-order/components/success/success.module.ts
+++ src/app/new-order/components/success/success.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {SuccessComponent} from './success.component'
+import {TuiButtonModule, TuiSvgModule} from '@taiga-ui/core'
 
 @NgModule({
   declarations: [SuccessComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TuiSvgModule, TuiButtonModule],
   exports: [SuccessComponent],
 })
 export class SuccessModule {}
